801e19f29e23b37f90adaf446f1c2a9ae8e95e42
Stephany Oliveira <disasxster@aol.com>
2022-07-19 22:02:37 -0300

    <feat>: inclusão da feature Filtro ao projeto

---
+++ src/components/Filtro/Filtro.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import styled from 'styled-components';
+
+// direcionar esses itens para o arquivo único de styled components (não esquecer do export)
+
+const Conteiner = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Titulo = styled.h3`
+  margin: 0 0 10px 0;
+`;
+
+const Formulario = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 10px;
+
+  label {
+     //screen reader only
+     position: absolute;
+        width: 1px;
+        height: 1px;
+        padding: 0;
+        margin: -1px;
+        overflow: hidden;
+        clip: rect(0,0,0,0);
+        border: 0;
+  }
+
+  input {
+    padding: 10px;
+    border-radius: 10px;
+    border: 2px solid gray;
+  }
+
+  input:hover {
+    border: 2px solid #87ceeb;
+    cursor: pointer;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+  }
+`;
+
+
+export const Filtro = (props) => {
+
+  return (
+    <Conteiner>
+      <Titulo>Busca</Titulo>
+      <Formulario>
+        <label>Filtrar por Nome do Produto</label>
+        <input type='text' value={props.inputNome} onChange={props.handleInputNome} placeholder='por Nome do Produto'/>
+        <label>Filtrar por Valor Mínimo</label>
+        <input type='number' value={props.inputMinValor} onChange={props.handleInputMinValor} placeholder='por Valor Mínimo'/>
+        <label>Filtrar por Valor Máximo</label>
+        <input type='number' value={props.inputMaxValor} onChange={props.handleInputMaxValor} placeholder='por Valor Máximo' />
+      </Formulario>
+    </Conteiner>
+  )
+}
--- src/components/Produtos/Produtos.js
+++ src/components/Produtos/Produtos.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import lista from "../MockDados/MockDados";
 import Cards from "../Card/Card";
 import "../Styles/Produtos.css";
 
-const Produtos = ({ handleClick }) => {
+const Produtos = (props) => {
   return (
     <section>
-      {lista.map((item) => (
-        <Cards key={item.id} item={item} handleClick={handleClick} />
+      {lista.filter((item)=>{
+          return item.nome.includes(props.inputNome)
+        }).filter((item)=>{
+          return item.preco >= props.inputMinValor
+        }).filter((item)=>{
+          return item.preco <= props.inputMaxValor
+        }).map((item) => (
+        <Cards key={item.id} item={item} handleClick={props.handleClick} />
       ))}
     </section>
   );
