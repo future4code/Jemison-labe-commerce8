4532e53abc1de6b7d9b2acdc28ce5837ede76c6c
sterx17 <disasxster@aol.com>
2022-07-21 20:10:42 -0300

    <style>:ajustes visuais nos cards

--- src/components/Card/styled.js
+++ src/components/Card/styled.js
@@ -2,40 +2,65 @@ import styled from "styled-components";
 
 
 export const Cardes = styled.div`
-width: 250px;
-padding: 20px;
+width: 85%;
+margin: 2vh auto;
+background-color: rgba(0, 0, 0,1);
+//padding: 10px;
+//border-radius: 10px;
+ // box-shadow: 
+    //5px 5px 12px 0 rgba(0, 0, 0, 0.10),
+    //-1px -1px 16px 0 rgba(255, 255, 255, 0.2)
+//  box-shadow: -1px 2px 24px 0 rgba(0, 0, 0, 0.2),
+//    2px -1px 24px 0 rgba(255, 255, 255, 0.5);
 `
+export const Flex = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2vh;
+`;
 
 export const Image = styled.img`
-width: 150px;
-height: 200px;
+width: 100%;
+border-radius: 5px;
 `
+export const Tag = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 2vh;
+`;
 
 export const Identificacao = styled.p`
 font-weight: bold;
 margin: 0;
+display: none;
 `
 
 export const Nome = styled.p`
-color: grey;
+color: white;
 font-weight: bold;
-margin: 0;
+margin: 0 5px 0 0;
 `
 
 export const Preco = styled.p`
-color: grey;
+color: white;
 margin: 0;
 `
 
 export const BotaoCard = styled.button`
+width: 100%;
 padding: 5px 10px;
 border: none;
 font-weight: bold;
 background-color: skyblue;
+// border-radius: 5px;
 outline: none;
 cursor: pointer;
+//box-shadow: -1px -1px 6px 0 rgba(0, 0, 0, 0.2),
+//    2px 2px 6px 0 rgba(255, 255, 255, 0.5);
 
 :active{
-    background-color: red;
+    background-color: skyblue;
 }
 `     
--- src/components/Produtos/styled.js
+++ src/components/Produtos/styled.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const ContPai = styled.section`
-width: 80%;
-margin: 5vw 0vw 0vw 15vw;
+width: 90%;
+margin: 5vh auto;
 display: grid;
 grid-template-columns: repeat(3, 1fr);
 grid-template-rows: auto;
+row-gap: 10px;
+column-gap: 1vw;
+// background-color: rgba(0, 0, 0,1);
 `
